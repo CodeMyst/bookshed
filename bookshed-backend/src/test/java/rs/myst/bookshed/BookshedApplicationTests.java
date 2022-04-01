e87b5044a764ec3156784eeee7511da4f4c3d62c
CodeMyst <code@myst.rs>
2022-04-01 18:06:36 +0200

    created an empty spring project

---
+++ bookshed-backend/src/test/java/rs/myst/bookshed/BookshedApplicationTests.java
@@ -0,0 +1,11 @@
+package rs.myst.bookshed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookshedApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
