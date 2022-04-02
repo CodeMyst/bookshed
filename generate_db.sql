8f10f1744cd7c953c55e07ed75356e3315ca1e78
CodeMyst <code@myst.rs>
2022-04-02 14:48:57 +0200

    added email to user schema

--- generate_db.sql
+++ generate_db.sql
@@ -2,8 +2,9 @@ create database if not exists bookshed;
 
 create table user
 (
-    username      varchar(50) not null primary key,
-    role          enum('ADMIN', 'USER') not null,
-    password_hash tinytext    not null,
-    created_at    datetime    not null
+    username      varchar(50)            not null primary key,
+    email         varchar(50)            not null,
+    role          enum ('ADMIN', 'USER') not null,
+    password_hash tinytext               not null,
+    created_at    datetime               not null
 );
