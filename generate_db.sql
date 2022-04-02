d91f30fada9b3e9cf7def3bdacdcb9215cd2a012
CodeMyst <code@myst.rs>
2022-04-02 14:45:29 +0200

    db schema for users

---
+++ generate_db.sql
@@ -0,0 +1,9 @@
+create database if not exists bookshed;
+
+create table user
+(
+    username      varchar(50) not null primary key,
+    role          enum('ADMIN', 'USER') not null,
+    password_hash tinytext    not null,
+    created_at    datetime    not null
+);
