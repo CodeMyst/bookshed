8d49516fe45f121b5ae7046a0943ef435486d004
marko <marko.tanasic01@gmail.com>
2022-04-11 15:52:33 +0200

    m

--- bookshed-frontend/src/app/components/navbar/navbar.component.ts
+++ bookshed-frontend/src/app/components/navbar/navbar.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { AuthResult, getUser, logout } from 'src/app/api/auth';
+import { Router } from '@angular/router';
+import { logout } from 'src/app/api/auth';
 import { GlobalConstants } from 'src/app/api/global.constants';
-import { User } from 'src/app/api/user';
+import { User, Role } from 'src/app/api/user';
 
 @Component({
   selector: 'app-navbar',
@@ -10,27 +11,31 @@ import { User } from 'src/app/api/user';
 })
 export class NavbarComponent implements OnInit {
 
-  currentUser: any;
+  currentUser: User | null = null;
+  userRole: string = "";
 
   onLogout: any;
   onLogin: any;
   checkIfLogged: any;
 
-  constructor() {
+  constructor(private router: Router) {
     this.onLogout = async () => {
       await logout();
 
       GlobalConstants.currentUser = null;
     };
 
-
     this.checkIfLogged = () => {
       this.currentUser = GlobalConstants.currentUser;
+      this.userRole = this.currentUser?.role === Role.USER ? "[U]" : "[A]";
     }
   }
 
+  goToPage(pageName: string) {
+    this.router.navigate([`${pageName}`]);
+  }
+
   ngOnInit() {
-    console.log("current user: " + this.currentUser);
   }
 
 }
