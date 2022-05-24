2b4a7d139daa249b0bc8da746919b7aeaee4d723
marko <marko.tanasic01@gmail.com>
2022-05-24 19:23:31 +0200

    added a special sticky post

--- bookshed-frontend/src/app/pages/forum/forum.component.ts
+++ bookshed-frontend/src/app/pages/forum/forum.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { isLoggedIn } from 'src/app/api/auth';
+import { getSelf, isLoggedIn } from 'src/app/api/auth';
 import { GlobalConstants } from 'src/app/api/global.constants';
 import { getPosts, getReplies, Post } from 'src/app/api/post';
+import { Role } from 'src/app/api/user';
 
 @Component({
   selector: 'app-forum',
@@ -11,19 +12,30 @@ import { getPosts, getReplies, Post } from 'src/app/api/post';
 export class ForumComponent implements OnInit {
 
   posts: Post[] = [];
+  stickyPost: Post | any;
   replies: number[] = [];
+
   isLoggedIn: boolean = false;
+  isAdmin: boolean = false;
+
+  setSticky: any;
 
   formatDate = GlobalConstants.formatDate;
 
-  constructor() { }
+  constructor() {
+    this.setSticky = () => {
+      GlobalConstants.isSticky = true;
+    }
+  }
 
   async ngOnInit() {
     this.isLoggedIn = await isLoggedIn();
+    this.isAdmin = (await getSelf()).role === Role.ADMIN;
 
     this.posts = await getPosts();
 
     this.posts.sort((a, b) => new Date(b.createdAt).valueOf() - new Date(a.createdAt).valueOf());
+    this.posts.sort(function(a, b) { return (a.sticky === b.sticky)? 0 : a.sticky? -1 : 1; })
 
     for (const post of this.posts) {
       this.replies.push((await getReplies(post.id)).length);
